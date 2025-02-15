a7e0c4b164fa189a2fc064df032a29df6c4b14c8
R0L0ct <facundoalemdev@gmail.com>
2025-02-15 19:22:56 -0300

    remove gutter background from kanagawa

--- lua/rolo/plugins/colorscheme.lua
+++ lua/rolo/plugins/colorscheme.lua
@@ -13,6 +13,15 @@ return {
 	config = function()
 		require("kanagawa").setup({
 			transparent = true,
+			colors = {
+				theme = {
+					all = {
+						ui = {
+							bg_gutter = "none",
+						},
+					},
+				},
+			},
 		})
 
 		vim.cmd("colorscheme kanagawa-dragon")
