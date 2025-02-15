624028dfcc3850104997d3df17e2cc97ae7608ed
R0L0ct <facundoalemdev@gmail.com>
2025-02-15 00:52:51 -0300

    change colorscheme to kanagawa-dragon

--- lua/rolo/plugins/colorscheme.lua
+++ lua/rolo/plugins/colorscheme.lua
@@ -1,18 +1,22 @@
+function ColorMyPencils(color)
+	color = color or "kanagawa-dragon"
+	vim.cmd.colorscheme(color)
+
+	vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+	vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+end
+
 return {
-	"rose-pine/neovim",
-	priority = 1000,
-    config = function()
-        function ColorMyPencils()
-            vim.cmd("colorscheme rose-pine")
+	"rebelot/kanagawa.nvim",
+	name = "kanagawa",
+	priority = 1000, -- High priority to ensure it loads early
+	config = function()
+		require("kanagawa").setup({
+			transparent = true,
+		})
 
-            vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-            vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+		vim.cmd("colorscheme kanagawa-dragon")
 
-            require("rose-pine").setup({
-                disable_background = true,
-            })
-        end
-
-        ColorMyPencils()
-    end,
+		ColorMyPencils()
+	end,
 }
