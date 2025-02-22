e00bc6334935299aea2585145a16c40403161052
R0L0ct <facundoalemdev@gmail.com>
2025-02-22 02:06:05 -0300

    add and config mason-tool-installer

--- lua/rolo/plugins/mason.lua
+++ lua/rolo/plugins/mason.lua
@@ -1,9 +1,13 @@
 return {
 	"williamboman/mason.nvim",
-	dependencies = "williamboman/mason-lspconfig.nvim",
+	dependencies = {
+		"williamboman/mason-lspconfig.nvim",
+		"WhoIsSethDaniel/mason-tool-installer.nvim",
+	},
 	config = function()
 		local mason = require("mason")
 		local mason_lspconfig = require("mason-lspconfig")
+		local mason_tool_installer = require("mason-tool-installer")
 
 		mason.setup()
 
@@ -22,6 +26,17 @@ return {
 				"rust_analyzer",
 			},
 			automatic_installation = true,
+		})
+
+		mason_tool_installer.setup({
+			ensure_installed = {
+				"prettier",
+				"stylua",
+				"black",
+				"isort",
+				"pylint",
+				"eslint_d",
+			},
 		})
 	end,
 }
