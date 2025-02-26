3a157db41814cf8b0687338dbb3494e3a7baa104
R0L0ct <facundoalemdev@gmail.com>
2025-02-26 17:56:21 -0300

    add telescope keymaps descriptions

--- lua/rolo/plugins/telescope.lua
+++ lua/rolo/plugins/telescope.lua
@@ -15,21 +15,21 @@ return {
 			},
 		})
 
-		vim.keymap.set("n", "<leader>pf", builtin.find_files, {})
-		vim.keymap.set("n", "<leader>pg", builtin.git_files, {})
+		vim.keymap.set("n", "<leader>pf", builtin.find_files, { desc = "Telescope find_files" })
+		vim.keymap.set("n", "<leader>pg", builtin.git_files, { desc = "Telescope git_files" })
 		vim.keymap.set("n", "<leader>ps", function()
 			builtin.grep_string({ search = vim.fn.input("Grep > ") })
-		end)
+		end, { desc = "Telescope grep_string Grep >" })
 		vim.keymap.set("n", "<leader>pe", builtin.buffers, { desc = "Telescope buffers" })
 		vim.keymap.set("n", "<leader>km", builtin.keymaps, { desc = "Telescope keymaps" })
 		vim.keymap.set("n", "<leader>pws", function()
 			local word = vim.fn.expand("<cword>")
 			builtin.grep_string({ search = word })
-		end)
+		end, { desc = "Telescope grep_string word under cursor" })
 		vim.keymap.set("n", "<leader>pWs", function()
 			local word = vim.fn.expand("<cWORD>")
 			builtin.grep_string({ search = word })
-		end)
-		vim.keymap.set("n", "<leader>ph", builtin.help_tags, {})
+		end, { desc = "Telescope grep_string full word" })
+		vim.keymap.set("n", "<leader>ph", builtin.help_tags, { desc = "Telescope help_tags" })
 	end,
 }
