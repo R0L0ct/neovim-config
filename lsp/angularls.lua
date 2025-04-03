2f1ceaff8e1b26f66ded377c5082ca39b1c3dc36
R0L0ct <facundoalemdev@gmail.com>
2025-04-03 17:05:25 -0300

    feat(lsp):correct lsp server config file names

--- lsp/angularls.lua
+++ lsp/angularls.lua
@@ -28,6 +28,13 @@ local config = {
 	on_new_config = function(new_config, new_root_dir)
 		new_config.cmd = cmd
 	end,
+	filetypes = {
+		"typescript",
+		"html",
+		"typescriptreact",
+		"typescript.tsx",
+		"htmlangular",
+	},
 }
 
 return config
