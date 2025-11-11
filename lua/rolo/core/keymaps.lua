ea3417a2cf445c6ce4580c912d94d08660855fc4
fnalem <fnalem@jusentrerios.gov.ar>
2025-11-11 12:56:20 +0000

    add more split keymaps

--- lua/rolo/core/keymaps.lua
+++ lua/rolo/core/keymaps.lua
@@ -24,6 +24,8 @@ keymap.set(
 	{ desc = "Delete other buffers but the current one" }
 )
 
+-- Splits
+
 keymap.set("n", "<leader>sv", "<C-w>v", { desc = "Split window vertically" })
 keymap.set("n", "<leader>sh", "<C-w>s", { desc = "Split window horizontally" })
 keymap.set("n", "<leader>se", "<C-w>=", { desc = "Make splits equal size" })
@@ -33,6 +35,25 @@ keymap.set("n", "<C-h>", "<C-w>h")
 keymap.set("n", "<C-j>", "<C-w>j")
 keymap.set("n", "<C-k>", "<C-w>k")
 keymap.set("n", "<C-l>", "<C-w>l")
+
+keymap.set("n", "<leader>sr", "<C-w>r", { desc = "Rotate splits" })
+keymap.set("n", "<leader>sV", "<C-w>t<C-w>H", { desc = "Make horizontal split vertical" })
+keymap.set("n", "<leader>sH", "<C-w>t<C-w>K", { desc = "Make vertical split horizontal" })
+
+keymap.set("n", "<A-Left>",  ":vertical resize -5<CR>", { desc = "Shrink width" })
+keymap.set("n", "<A-Right>", ":vertical resize +5<CR>", { desc = "Increase width" })
+keymap.set("n", "<A-Up>",    ":resize -2<CR>", { desc = "Shrink height" })
+keymap.set("n", "<A-Down>",  ":resize +2<CR>", { desc = "Increase height" })
+
+keymap.set("n", "<A-h>", "<C-w>H", { desc = "Move split to left" })
+keymap.set("n", "<A-j>", "<C-w>J", { desc = "Move split to bottom" })
+keymap.set("n", "<A-k>", "<C-w>K", { desc = "Move split to top" })
+keymap.set("n", "<A-l>", "<C-w>L", { desc = "Move split to right" })
+
+-- Auto-resize cuando cambia el tamaño de la ventana
+vim.api.nvim_create_autocmd("VimResized", {
+  command = "wincmd =",
+})
 
 -- Terminal
 local opts = { noremap = true, silent = true }
