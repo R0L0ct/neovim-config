2727e55e6c61fc7b27f0a6fb960099da52acf044
fnalem <fnalem@jusentrerios.gov.ar>
2025-11-14 09:59:27 -0300

    feat: add gitsigns & harpoon keymaps description

--- lua/rolo/plugins/harpoon.lua
+++ lua/rolo/plugins/harpoon.lua
@@ -1,16 +1,24 @@
 return {
-  "ThePrimeagen/harpoon",
-  config = function()
-    local mark = require("harpoon.mark")
-    local ui = require("harpoon.ui")
-    local keymap = vim.keymap
+	"ThePrimeagen/harpoon",
+	config = function()
+		local mark = require("harpoon.mark")
+		local ui = require("harpoon.ui")
+		local keymap = vim.keymap
 
-    keymap.set("n", "<leader>ha", mark.add_file)
-    keymap.set("n", "<leader>hh", ui.toggle_quick_menu)
+		keymap.set("n", "<leader>ha", mark.add_file, { desc = "Harpoon add file" })
+		keymap.set("n", "<leader>hh", ui.toggle_quick_menu, { desc = "Harpoon toggle quick menu" })
 
-    keymap.set("n", "<leader>hq", function() ui.nav_file(1) end)
-    keymap.set("n", "<leader>hw", function() ui.nav_file(2) end)
-    keymap.set("n", "<leader>he", function() ui.nav_file(3) end)
-    keymap.set("n", "<leader>hr", function() ui.nav_file(4) end)
-  end
+		keymap.set("n", "<leader>hq", function()
+			ui.nav_file(1)
+		end, { desc = "Harpoon nav file 1" })
+		keymap.set("n", "<leader>hw", function()
+			ui.nav_file(2)
+		end, { desc = "Harpoon nav file 2" })
+		keymap.set("n", "<leader>he", function()
+			ui.nav_file(3)
+		end, { desc = "Harpoon nav file 3" })
+		keymap.set("n", "<leader>hr", function()
+			ui.nav_file(4)
+		end, { desc = "Harpoon nav file 4" })
+	end,
 }
